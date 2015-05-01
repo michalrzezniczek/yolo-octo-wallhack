7d136e81794819d45395681b1ca752c22f249a71
Michal Rzezniczek <michal.rzezniczek@outlook.com>
2015-05-01 21:36:37 +0200

    Revert "Form validation, catching user's input"
    This reverts commit bf05eb4c92d6e547bfb0a3468c91c486cfbc7e4b.

--- OJPClient/Form1.h
+++ OJPClient/Form1.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <string>
+
 namespace WindowsFormApplication1 {
 
 	using namespace System;
@@ -34,9 +34,7 @@ namespace WindowsFormApplication1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^  textServerIP;
-	protected:
-
+	private: System::Windows::Forms::Label^  labelServerIP;
 	private: System::Windows::Forms::MaskedTextBox^  textBoxServerIP0;
 	private: System::Windows::Forms::MaskedTextBox^  textBoxServerIP1;
 	private: System::Windows::Forms::MaskedTextBox^  textBoxServerIP2;
@@ -56,11 +54,7 @@ namespace WindowsFormApplication1 {
 	protected:
 
 	private:
-		System::String ^address;
-		Byte adr0 = 0, adr1 = 0, adr2 = 0, adr3 = 0;
-	private: System::Windows::Forms::Label^  labelServerIP;
-
-			 /// <summary>
+		/// <summary>
 		/// Required designer variable.
 		/// </summary>
 		System::ComponentModel::Container ^components;
@@ -72,7 +66,7 @@ namespace WindowsFormApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->textServerIP = (gcnew System::Windows::Forms::Label());
+			this->labelServerIP = (gcnew System::Windows::Forms::Label());
 			this->textBoxServerIP0 = (gcnew System::Windows::Forms::MaskedTextBox());
 			this->textBoxServerIP1 = (gcnew System::Windows::Forms::MaskedTextBox());
 			this->textBoxServerIP2 = (gcnew System::Windows::Forms::MaskedTextBox());
@@ -81,59 +75,44 @@ namespace WindowsFormApplication1 {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->buttonSave = (gcnew System::Windows::Forms::Button());
-			this->labelServerIP = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
-			// textServerIP
+			// labelServerIP
 			// 
-			this->textServerIP->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+			this->labelServerIP->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->textServerIP->Font = (gcnew System::Drawing::Font( L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->labelServerIP->Font = (gcnew System::Drawing::Font( L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238) ));
-			this->textServerIP->Location = System::Drawing::Point( 13, 13 );
-			this->textServerIP->Name = L"textServerIP";
-			this->textServerIP->Size = System::Drawing::Size( 101, 23 );
-			this->textServerIP->TabIndex = 0;
-			this->textServerIP->Text = L"Server IP:";
+			this->labelServerIP->Location = System::Drawing::Point( 13, 13 );
+			this->labelServerIP->Name = L"labelServerIP";
+			this->labelServerIP->Size = System::Drawing::Size( 227, 23 );
+			this->labelServerIP->TabIndex = 0;
+			this->labelServerIP->Text = L"Server IP:";
 			// 
 			// textBoxServerIP0
 			// 
 			this->textBoxServerIP0->Font = (gcnew System::Drawing::Font( L"Microsoft Sans Serif", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238) ));
-			this->textBoxServerIP0->ImeMode = System::Windows::Forms::ImeMode::NoControl;
 			this->textBoxServerIP0->Location = System::Drawing::Point( 18, 40 );
 			this->textBoxServerIP0->Mask = L"000";
 			this->textBoxServerIP0->Name = L"textBoxServerIP0";
-			this->textBoxServerIP0->ResetOnPrompt = false;
-			this->textBoxServerIP0->ResetOnSpace = false;
 			this->textBoxServerIP0->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
 			this->textBoxServerIP0->Size = System::Drawing::Size( 51, 38 );
-			this->textBoxServerIP0->SkipLiterals = false;
 			this->textBoxServerIP0->TabIndex = 1;
 			this->textBoxServerIP0->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
-			this->textBoxServerIP0->TextChanged += gcnew System::EventHandler( this, &Form1::textBoxServerIP0_TextChanged );
-			this->textBoxServerIP0->Enter += gcnew System::EventHandler( this, &Form1::textBoxServerIP0_Enter );
-			this->textBoxServerIP0->Validated += gcnew System::EventHandler( this, &Form1::textBoxServerIP0_Validated );
 			// 
 			// textBoxServerIP1
 			// 
-			this->textBoxServerIP1->BeepOnError = true;
 			this->textBoxServerIP1->Font = (gcnew System::Drawing::Font( L"Microsoft Sans Serif", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238) ));
 			this->textBoxServerIP1->Location = System::Drawing::Point( 75, 39 );
 			this->textBoxServerIP1->Mask = L"000";
 			this->textBoxServerIP1->Name = L"textBoxServerIP1";
-			this->textBoxServerIP1->ResetOnPrompt = false;
-			this->textBoxServerIP1->ResetOnSpace = false;
 			this->textBoxServerIP1->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
 			this->textBoxServerIP1->Size = System::Drawing::Size( 51, 38 );
-			this->textBoxServerIP1->SkipLiterals = false;
 			this->textBoxServerIP1->TabIndex = 2;
 			this->textBoxServerIP1->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
-			this->textBoxServerIP1->TextChanged += gcnew System::EventHandler( this, &Form1::textBoxServerIP1_TextChanged );
-			this->textBoxServerIP1->Enter += gcnew System::EventHandler( this, &Form1::textBoxServerIP1_Enter );
-			this->textBoxServerIP1->Validated += gcnew System::EventHandler( this, &Form1::textBoxServerIP1_Validated );
 			// 
 			// textBoxServerIP2
 			// 
@@ -146,9 +125,6 @@ namespace WindowsFormApplication1 {
 			this->textBoxServerIP2->Size = System::Drawing::Size( 51, 38 );
 			this->textBoxServerIP2->TabIndex = 3;
 			this->textBoxServerIP2->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
-			this->textBoxServerIP2->TextChanged += gcnew System::EventHandler( this, &Form1::textBoxServerIP2_TextChanged );
-			this->textBoxServerIP2->Enter += gcnew System::EventHandler( this, &Form1::textBoxServerIP2_Enter );
-			this->textBoxServerIP2->Validated += gcnew System::EventHandler( this, &Form1::textBoxServerIP2_Validated );
 			// 
 			// textBoxServerIP3
 			// 
@@ -161,9 +137,6 @@ namespace WindowsFormApplication1 {
 			this->textBoxServerIP3->Size = System::Drawing::Size( 51, 38 );
 			this->textBoxServerIP3->TabIndex = 4;
 			this->textBoxServerIP3->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
-			this->textBoxServerIP3->TextChanged += gcnew System::EventHandler( this, &Form1::textBoxServerIP3_TextChanged );
-			this->textBoxServerIP3->Enter += gcnew System::EventHandler( this, &Form1::textBoxServerIP3_Enter );
-			this->textBoxServerIP3->Validated += gcnew System::EventHandler( this, &Form1::textBoxServerIP3_Validated );
 			// 
 			// labelDot
 			// 
@@ -209,31 +182,18 @@ namespace WindowsFormApplication1 {
 			this->buttonSave->TabIndex = 8;
 			this->buttonSave->Text = L"Save";
 			this->buttonSave->UseVisualStyleBackColor = true;
-			this->buttonSave->Click += gcnew System::EventHandler( this, &Form1::buttonSave_Click );
-			// 
-			// labelServerIP
-			// 
-			this->labelServerIP->AutoSize = true;
-			this->labelServerIP->Font = (gcnew System::Drawing::Font( L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(238) ));
-			this->labelServerIP->Location = System::Drawing::Point( 120, 13 );
-			this->labelServerIP->Name = L"labelServerIP";
-			this->labelServerIP->Size = System::Drawing::Size( 0, 25 );
-			this->labelServerIP->TabIndex = 9;
-			this->labelServerIP->Visible = false;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF( 6, 13 );
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size( 257, 119 );
-			this->Controls->Add( this->labelServerIP );
 			this->Controls->Add( this->buttonSave );
 			this->Controls->Add( this->textBoxServerIP3 );
 			this->Controls->Add( this->textBoxServerIP2 );
 			this->Controls->Add( this->textBoxServerIP1 );
 			this->Controls->Add( this->textBoxServerIP0 );
-			this->Controls->Add( this->textServerIP );
+			this->Controls->Add( this->labelServerIP );
 			this->Controls->Add( this->labelDot );
 			this->Controls->Add( this->label1 );
 			this->Controls->Add( this->label2 );
@@ -246,114 +206,7 @@ namespace WindowsFormApplication1 {
 
 		}
 #pragma endregion
-		
-		
-		private: System::Void ClearTextBox( System::Object^ sender )
-		{
-			if ( static_cast<MaskedTextBox^>(sender)->Text == "" ) {
-				static_cast<MaskedTextBox^>(sender)->Clear();
-			}
-		}
-		private: System::Byte AfterValidation( System::Object ^ sender )
-		{
-			Byte tmp;
-			if ( static_cast<MaskedTextBox^>(sender)->Text->Length > 0 ) {
-				static_cast<MaskedTextBox^>(sender)->Text = static_cast<MaskedTextBox^>(sender)->Text->Replace( " ", "" );
-				if ( System::Convert::ToInt32( static_cast<MaskedTextBox^>(sender)->Text ) <= 255 ) {
-					return System::Convert::ToByte( static_cast<MaskedTextBox^>(sender)->Text );
-				}
-				else {
-					static_cast<MaskedTextBox^>(sender)->Text = "000";
-				}
-				return 0;
-			}
-		}
 
-		//
-		// textBoxServerIP0
-		//
-		private: System::Void textBoxServerIP0_Enter( System::Object^  sender, System::EventArgs^  e )
-		{
-			ClearTextBox( textBoxServerIP0 );
-
-		}
-		private: System::Void textBoxServerIP0_Validated( System::Object^  sender, System::EventArgs^  e )
-		{
-			adr0 = AfterValidation( textBoxServerIP0 );
-		}
-
-		private: System::Void textBoxServerIP0_TextChanged( System::Object^  sender, System::EventArgs^  e )
-		{
-			if ( textBoxServerIP0->Text->Length == 3 ) {
-				ProcessTabKey( true );
-			}
-		}
-
-		//
-		// textBoxServerIP1
-		//
-		private: System::Void textBoxServerIP1_Enter( System::Object^  sender, System::EventArgs^  e )
-		{
-			ClearTextBox( textBoxServerIP1 );
-
-		}
-		private: System::Void textBoxServerIP1_Validated( System::Object^  sender, System::EventArgs^  e )
-		{
-			adr1 = AfterValidation( textBoxServerIP1 );
-		}
-
-		private: System::Void textBoxServerIP1_TextChanged( System::Object^  sender, System::EventArgs^  e )
-		{
-			if ( textBoxServerIP1->Text->Length == 3 ) {
-				ProcessTabKey( true );
-			}
-		}
-		
-		//
-		// textBoxServerIP2
-		//
-		private: System::Void textBoxServerIP2_Enter( System::Object^  sender, System::EventArgs^  e )
-		{
-			ClearTextBox( textBoxServerIP2 );
-
-		}
-		private: System::Void textBoxServerIP2_Validated( System::Object^  sender, System::EventArgs^  e )
-		{
-			adr2 = AfterValidation( textBoxServerIP2 );
-		}
-
-		private: System::Void textBoxServerIP2_TextChanged( System::Object^  sender, System::EventArgs^  e )
-		{
-			if ( textBoxServerIP2->Text->Length == 3 ) {
-				ProcessTabKey( true );
-			}
-		}
-
-		//
-		// textBoxServerIP3
-		//
-		private: System::Void textBoxServerIP3_Enter( System::Object^  sender, System::EventArgs^  e )
-		{
-			ClearTextBox( textBoxServerIP3 );
-
-		}
-		private: System::Void textBoxServerIP3_Validated( System::Object^  sender, System::EventArgs^  e )
-		{
-			adr3 = AfterValidation( textBoxServerIP3 );
-		}
-
-		private: System::Void textBoxServerIP3_TextChanged( System::Object^  sender, System::EventArgs^  e )
-		{
-			if ( textBoxServerIP3->Text->Length == 3 ) {
-				ProcessTabKey( true );
-			}
-		}
-		private: System::Void buttonSave_Click( System::Object^  sender, System::EventArgs^  e )
-		{
-			address = (adr0.ToString() + "." + adr1.ToString() + "." + adr2.ToString() + "." + adr3.ToString());
-			labelServerIP->Text = address;
-			labelServerIP->Visible = true;
-		}
 };
 }
 
