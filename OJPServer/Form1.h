d34462fe836a399cb34101407a8a77305f9a56df
Michal Rzezniczek <michal.rzezniczek@outlook.com>
2015-05-31 16:56:16 +0200

    Add dataGridView; Fix IP validation regex

--- OJPServer/Form1.h
+++ OJPServer/Form1.h
@@ -41,6 +41,11 @@ namespace WindowsFormApplication1 {
 	private: System::Windows::Forms::TextBox^  textBoxIP;
 	private: System::Windows::Forms::TabPage^  tabPageList;
 	private: System::Windows::Forms::Label^  labelStatusOfConnection;
+	private: System::Windows::Forms::DataGridView^  dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  HostName;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  IP;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Date;
+
 
 	protected:
 
@@ -69,15 +74,21 @@ namespace WindowsFormApplication1 {
 			this->buttonCollectData = (gcnew System::Windows::Forms::Button());
 			this->textBoxIP = (gcnew System::Windows::Forms::TextBox());
 			this->tabPageList = (gcnew System::Windows::Forms::TabPage());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->HostName = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->IP = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Date = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->tabControl1->SuspendLayout();
 			this->tabPageCollect->SuspendLayout();
+			this->tabPageList->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// tabControl1
 			// 
 			this->tabControl1->Controls->Add( this->tabPageCollect );
 			this->tabControl1->Controls->Add( this->tabPageList );
-			this->tabControl1->Location = System::Drawing::Point( 1, 1 );
+			this->tabControl1->Location = System::Drawing::Point( 0, 0 );
 			this->tabControl1->Name = L"tabControl1";
 			this->tabControl1->SelectedIndex = 0;
 			this->tabControl1->Size = System::Drawing::Size( 286, 263 );
@@ -128,6 +139,7 @@ namespace WindowsFormApplication1 {
 			// 
 			// tabPageList
 			// 
+			this->tabPageList->Controls->Add( this->dataGridView1 );
 			this->tabPageList->Location = System::Drawing::Point( 4, 22 );
 			this->tabPageList->Name = L"tabPageList";
 			this->tabPageList->Padding = System::Windows::Forms::Padding( 3 );
@@ -136,17 +148,57 @@ namespace WindowsFormApplication1 {
 			this->tabPageList->Text = L"List";
 			this->tabPageList->UseVisualStyleBackColor = true;
 			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange( gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >( 3 )
+			{
+				this->HostName,
+					this->IP, this->Date
+			} );
+			this->dataGridView1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->dataGridView1->Location = System::Drawing::Point( 3, 3 );
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size( 272, 231 );
+			this->dataGridView1->TabIndex = 0;
+			// 
+			// HostName
+			// 
+			this->HostName->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::Fill;
+			this->HostName->HeaderText = L"HostName";
+			this->HostName->Name = L"HostName";
+			this->HostName->Resizable = System::Windows::Forms::DataGridViewTriState::True;
+			// 
+			// IP
+			// 
+			this->IP->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::Fill;
+			this->IP->HeaderText = L"IP";
+			this->IP->Name = L"IP";
+			this->IP->Resizable = System::Windows::Forms::DataGridViewTriState::False;
+			// 
+			// Date
+			// 
+			this->Date->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::AllCells;
+			this->Date->HeaderText = L"Date";
+			this->Date->Name = L"Date";
+			this->Date->Resizable = System::Windows::Forms::DataGridViewTriState::False;
+			this->Date->Width = 55;
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF( 6, 13 );
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size( 284, 261 );
 			this->Controls->Add( this->tabControl1 );
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"Form1";
 			this->Text = L"[OJP] Manager";
 			this->tabControl1->ResumeLayout( false );
 			this->tabPageCollect->ResumeLayout( false );
 			this->tabPageCollect->PerformLayout();
+			this->tabPageList->ResumeLayout( false );
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout( false );
 
 		}
@@ -170,7 +222,7 @@ namespace WindowsFormApplication1 {
 			System::String ^address = textBoxIP->Text;
 			System::Int32 tmp;
 
-			System::String ^patternRegex = "[0-2][0-9][0-9][.][0-2][0-9][0-9][.][0-2][0-9][0-9][.][0-2][0-9][0-9]";
+			System::String ^patternRegex = "([0-2]|[0-9][0-9]|[0-2][0-9][0-9])[.]([0-2]|[0-9][0-9]|[0-2][0-9][0-9])[.]([0-2]|[0-9][0-9]|[0-2][0-9][0-9])[.]([0-2]|[0-9][0-9]|[0-2][0-9][0-9])";
 
 			System::Text::RegularExpressions::Regex ^Regex = gcnew System::Text::RegularExpressions::Regex( patternRegex );
 
